5e000ea2af2636d0e62963c6ca91d47831e21431
AIL-PC-307-Windows <aakash.gupta@argusoft.com>
2026-08-17 14:47:03 +0530

    Updated with generic code for print messafe

--- GroupByPractice/Program.cs
+++ GroupByPractice/Program.cs
@@ -17,7 +17,7 @@ class Program
            new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= [97, 72, 81, 60]},
            new Student {First="Claire", Last="O'Donnell", ID=112, Scores= [75, 84, 91, 39]},
            new Student {First="Sven", Last="Mortensen", ID=113, Scores= [99, 89, 91, 95]},
-           new Student {First="Cesar", Last="Garcia", ID=114, Scores= [72, 81, 65, 84]},
+           new Student {First="Sven", Last="Garcia", ID=114, Scores= [72, 81, 65, 84]},
            new Student {First="Debra", Last="Garcia", ID=115, Scores= [97, 89, 85, 82]}
         ];
 
@@ -33,7 +33,7 @@ class Program
         from student in students
             group student by student.Scores.Average() >= 80;
         
-        PrintBooleanQuery(booleanGroupQuery); // print the query
+        PrintGroups(booleanGroupQuery, "Boolean grouping:", key => key ? "High averages" : "Low averages");
 
         // Grouping by numeric range
         var numericGroupQuery = 
@@ -42,31 +42,26 @@ class Program
             orderby g.Key
             select g;
 
-        PrintNumericQuery(numericGroupQuery);
-    }
-    static void PrintBooleanQuery(IEnumerable<IGrouping<bool, Student>>  booleanGroupQuery)
-    {
-        Console.WriteLine("\n\n\n");
-        foreach (var studentGroup in booleanGroupQuery)
-        {
-            Console.WriteLine(studentGroup.Key == true ? "High averages" : "Low averages");
-            foreach (var student in studentGroup)
-            {
-                Console.WriteLine("   {0}, {1}:{2}", student.Last, student.First, student.Scores.Average());
-            }
-        }
+        PrintGroups(numericGroupQuery, "Numeric range grouping:", key => $"Students with an average between {key * 10} and {key * 10 + 10}");
+
+        // grouping by wrt a composite key
+        var compositeGroupQuery = 
+            from student in students
+            group student by new {student.First};
+
+        PrintGroups(compositeGroupQuery, "Composite Query groups by student's First Name: ", key => $"Key: {key}");
+        
     }
 
-    static void PrintNumericQuery(IEnumerable<IGrouping<int, Student>> studentQuery)
-    {   
-        Console.WriteLine("\n\n\n");
-        foreach (var studentGroup in studentQuery)
+    static void PrintGroups<TKey>(IEnumerable<IGrouping<TKey,Student>>query, string message, Func<TKey, string> describeKey)
+    {
+        Console.WriteLine($"\n\n\n{message}");
+        foreach (var item in query)
         {
-            int temp = studentGroup.Key * 10;
-            Console.WriteLine($"Students with an average between {temp} and {temp + 10}");
-            foreach (var student in studentGroup)
+            Console.WriteLine(describeKey(item.Key));
+            foreach (var student in item)
             {
-                Console.WriteLine("   {0}, {1}:{2}", student.Last, student.First, student.Scores.Average());
+                Console.WriteLine($" {student.First} {student.Last}: {student.Scores.Average()}");
             }
         }
     }
